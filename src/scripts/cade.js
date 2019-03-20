1feba1a0c6b4b6c503e32025c3b2bcfa42b49512
guoshuaifeng <guoshuaifeng@laipaiya.com>
2019-03-20 17:22:16 +0800

    更改绘线的实现方式
    不再使用监听鼠标的绘线方式，采用生成一个虚拟的点，然后采用拖动点的方式来监听事件，实现绘线

--- src/scripts/cade.js
+++ src/scripts/cade.js
@@ -19,6 +19,10 @@ class Cade {
       width: stageWidth,
       height: stageHeight
     });
+    this.blockLayer = new Konva.Layer();
+    this.lineLayer = new Konva.Layer();
+    this.stage.add(this.blockLayer);
+    this.stage.add(this.lineLayer);
   }
 
   // 生成唯一ID
@@ -32,13 +36,11 @@ class Cade {
 
   // 创建块
   drawBlock(config) {
-    const layer = new Konva.Layer({
+    const blockGroup = new Konva.Group({
       x: config.x,
       y: config.y,
-      draggable: true,
-      id: this.randomID()
+      draggable: true
     });
-    const blockGroup = new Konva.Group();
     // 绘制文字
     const blockText = new Konva.Text({
       x: 0,
@@ -62,7 +64,7 @@ class Cade {
       shadowColor: '#cdcdcd',
       shadowBlur: 10,
       shadowOffset: { x: 0, y: 0 },
-      shadowOpacity: 0.3
+      shadowOpacity: 0.5
     });
     blockText.on('mouseenter', () => {
       this.stage.container().style.cursor = 'move';
@@ -72,9 +74,9 @@ class Cade {
     });
     blockGroup.add(rect);
     blockGroup.add(blockText);
-    layer.add(blockGroup);
-    layer.add(this.drawPoints(rect));
-    this.stage.add(layer);
+    blockGroup.add(this.drawPoints(rect));
+    this.blockLayer.add(blockGroup);
+    this.blockLayer.draw();
   }
 
   // 绘制连接点
@@ -86,7 +88,8 @@ class Cade {
     const _attr = _ctx.attrs;
     // 单个点合集
     const pointGroup = new Konva.Group({
-      id: this.randomID()
+      id: this.randomID(),
+      name: 'pointGroup'
     });
     const pointRing = new Konva.Ring({
       x: 0,
@@ -107,16 +110,16 @@ class Cade {
       name: 'pointCircle'
     });
     // 鼠标移入时填充蓝色，移出时填充白色
-    pointCircle.on('mouseenter', evt => {
+    pointCircle.on('mouseenter', event => {
       // 当前如果是在画线，则不设置监听
       if (this.lineDrawing) return false;
-      evt.target.fill(strokeColor);
-      evt.currentTarget.parent.draw();
+      event.target.fill(strokeColor);
+      event.currentTarget.parent.draw();
     });
-    pointCircle.on('mouseleave', evt => {
+    pointCircle.on('mouseleave', event => {
       if (this.lineDrawing) return false;
-      evt.target.fill('white');
-      evt.currentTarget.parent.draw();
+      event.target.fill('white');
+      event.currentTarget.parent.draw();
     });
     pointGroup.add(pointRing);
     pointGroup.add(pointCircle);
@@ -134,43 +137,62 @@ class Cade {
         y: axisArray[i].y,
         id: this.randomID()
       });
-      // 鼠标按下时设置对鼠标移动的监听
-      _pointGroupClone.on('mousedown', evt => {
-        evt.cancelBubble = true;
-        // 获取到当前point的中心位置
-        this.lineStartPoint = evt.currentTarget;
-        this.drawDashLine(evt.currentTarget.absolutePosition());
-        this.pointRingVisiableSwitch(true, evt.currentTarget.attrs.id);
-      });
-      _pointGroupClone.on('mouseenter', evt => {
+      _pointGroupClone.on('mouseenter', enterEvent => {
+        this.lineStartPoint = enterEvent.currentTarget;
+        // 判断当前是否处于绘线状态，如果不是，则创建dropCircle，并添加拖拽事件监听
         if (this.lineDrawing) {
         } else {
+          const startPointTarget = this.lineStartPoint.absolutePosition();
+          let dropCircleGroup = this.stage.find('#dropCircleGroup');
+          if (dropCircleGroup.length > 0) {
+            dropCircleGroup.each(dcItem => {
+              dcItem.destroy();
+            });
+          }
+          dropCircleGroup = new Konva.Group({
+            x: startPointTarget.x,
+            y: startPointTarget.y,
+            draggable: true,
+            id: 'dropCircleGroup'
+          });
           // 用于拖拽的点
           const dropCircle = new Konva.Circle({
-            x: 0,
-            y: 0,
             radius: 5,
             fill: 'red',
             stroke: strokeColor,
             strokeWidth: 1,
-            draggable: true,
-            id: 'dropCircle'
+            name: 'dropCircle'
           });
-          dropCircle.on('dragmove', evt => {});
-          _pointGroupClone.add(dropCircle);
+          const dropCircleRing = new Konva.Ring({
+            innerRadius: 5,
+            outerRadius: 11,
+            fill: strokeColor,
+            opacity: 0,
+            name: 'dropCircleRing'
+          });
+          const lineID = this.randomID();
+          dropCircleGroup.on('dragstart', () => {
+            this.lineDrawing = true;
+            this.pointRingVisiableSwitch(true, enterEvent.currentTarget.attrs.id);
+          });
+          dropCircleGroup.on('dragmove', event => {
+            const gropPosition = event.currentTarget.absolutePosition();
+            this.drawDashLine([startPointTarget.x, startPointTarget.y, gropPosition.x, gropPosition.y], lineID);
+          });
+          dropCircleGroup.on('dragend', () => {
+            this.pointRingVisiableSwitch(false, enterEvent.currentTarget.attrs.id);
+          });
+          dropCircleGroup.add(dropCircle);
+          dropCircleGroup.add(dropCircleRing);
+          this.lineLayer.add(dropCircleGroup);
+          this.lineLayer.on('drop', () => {
+            console.log('hi');
+          });
+          this.lineLayer.draw();
         }
-
-        evt.cancelBubble = true;
-        if (!this.lineDrawing) return (this.stage.container().style.cursor = 'crosshair');
-        // 只有当画线时触发以下操作
-        this.lineEndPoint = evt.currentTarget;
       });
-      _pointGroupClone.on('mouseleave', evt => {
-        evt.cancelBubble = true;
-        if (!this.lineDrawing) return (this.stage.container().style.cursor = 'default');
-        // 只有当画线时触发以下操作
-        console.count();
-        // this.lineEndPoint = undefined;
+      _pointGroupClone.on('mouseleave', event => {
+        event.cancelBubble = true;
       });
       pointsGroup.add(_pointGroupClone);
     }
@@ -179,63 +201,53 @@ class Cade {
 
   // point环的显隐
   pointRingVisiableSwitch(visiable, layerID = null) {
-    this.stage.find('.pointRing').each(item => {
+    this.blockLayer.find('.pointRing').each(item => {
       if (item.parent.attrs.id != layerID) {
         item.opacity(visiable ? 0.5 : 0);
       }
     });
-    this.stage.find('.pointsGroup').each(gItem => {
-      gItem.parent.draw();
-    });
+    this.blockLayer.draw();
   }
 
   // 绘制连线
-  drawDashLine(_currentPos) {
-    // 为每一条线设置唯一ID
-    const lineID = this.randomID();
-    this.stage.on('mousemove', moveEvt => {
-      this.lineDrawing = true;
-      const _pointsArray = [_currentPos.x, _currentPos.y, moveEvt.evt.offsetX, moveEvt.evt.offsetY];
-      this.stage.container().style.cursor = 'crosshair';
-      const lineByID = this.stage.find(`#${lineID}`);
-      if (lineByID.length > 0) {
-        lineByID[0].attrs.points = _pointsArray;
-        lineByID[0].parent.draw();
-      } else {
-        const layer = new Konva.Layer();
-        const line = new Konva.Line({
-          points: _pointsArray,
-          stroke: strokeColor,
-          lineCap: 'round',
-          lineJoin: 'round',
-          dash: [5],
-          id: lineID
-        });
-        layer.add(line);
-        this.stage.add(layer);
-      }
-    });
+  drawDashLine(_points, _lineID) {
+    const _pointsArray = _points;
+    this.stage.container().style.cursor = 'crosshair';
+    const lineByID = this.lineLayer.find(`#${_lineID}`);
+    if (lineByID.length > 0) {
+      lineByID[0].attrs.points = _pointsArray;
+      lineByID[0].parent.draw();
+    } else {
+      const line = new Konva.Line({
+        points: _pointsArray,
+        stroke: strokeColor,
+        lineCap: 'round',
+        lineJoin: 'round',
+        dash: [5],
+        id: _lineID
+      });
+      this.lineLayer.add(line);
+    }
 
     this.stage.on('mouseup', () => {
-      // 鼠标抬起时销毁鼠标移动的监听
-      this.stage.off('mousemove mouseup');
-      // 取消绘线状态
-      this.lineDrawing = false;
-      // 设置鼠标样式
-      this.stage.container().style.cursor = 'default';
-      // 消除pointRing样式
-      this.pointRingVisiableSwitch(false);
-      this.lineStartPoint.find('.pointCircle')[0].fill('white');
-      this.stage.find(`#${lineID}`)[0].destroy();
-      if (this.lineEndPoint && this.lineStartPoint.attrs.id !== this.lineEndPoint.attrs.id) {
-        this.drawArrowLine();
-      }
-      this.stage.draw();
+      // // 鼠标抬起时销毁鼠标移动的监听
+      // this.stage.off('mousemove mouseup');
+      // // 取消绘线状态
+      // this.lineDrawing = false;
+      // // 设置鼠标样式
+      // this.stage.container().style.cursor = 'default';
+      // // 消除pointRing样式
+      // this.pointRingVisiableSwitch(false);
+      // this.lineStartPoint.find('.pointCircle')[0].fill('white');
+      // this.stage.find(`#${_lineID}`)[0].destroy();
+      // if (this.lineEndPoint && this.lineStartPoint.attrs.id !== this.lineEndPoint.attrs.id) {
+      //   this.drawArrowLine();
+      // }
+      // this.stage.draw();
     });
   }
 
   drawArrowLine() {
-    const layer = new Konva.Layer();
     const line = new Konva.Arrow({
       points: [
         this.lineStartPoint.absolutePosition().x,
@@ -249,8 +261,7 @@ class Cade {
       pointerWidth: 5,
       id: this.randomID()
     });
-    layer.add(line);
-    this.stage.add(layer);
+    this.lineLayer.add(line);
   }
 }
 
